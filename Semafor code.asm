7a06d97b4920e13d58f81d399ff921e6252466c1
Tamara Ristova <tamararistova11@hotmail.com>
2024-05-31 22:09:00 +0200

    Create Semafor code.asm

---
+++ Semafor code.asm
@@ -0,0 +1,32 @@
+
+START MVI H,LIGHT
+
+MVI L, LOC ;
+
+MOV A,M ; 
+
+OUT PORTA ; 
+
+CALL DOCNI_1MIN ;
+
+INX H ; 
+
+MOV A,M ; 
+
+OUT PORTA ; 
+
+CALL DOCNI_3SEC ;
+
+-
+
+-
+
+MOV A,M ; 
+
+OUT PORTA ; 
+
+CALL DOCNI_3SEC ;
+
+JMP START ;
+
+END 
